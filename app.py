a898624fd68562a5530538c507fe030e46b68b66
katsiaryna-lysenka <katya.lysenko.1999@gmail.com>
2024-01-22 13:31:07 +0300

    Commit

--- app.py
+++ app.py
@@ -9,15 +9,12 @@ from tensorflow.keras.saving import save_model
 
 app = Flask(__name__)
 
-# Загрузка данных
 (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=10000)
 
-# Предобработка данных
 maxlen = 256
 x_train = pad_sequences(x_train, maxlen=maxlen, padding='post')
 x_test = pad_sequences(x_test, maxlen=maxlen, padding='post')
 
-# Создание модели
 model = Sequential([
     Embedding(10000, 64, trainable=False),
     GlobalAveragePooling1D(),
@@ -27,27 +24,24 @@ model = Sequential([
     Dense(1, activation='sigmoid')
 ])
 
-# Компиляция модели с оптимизатором Adam и установкой learning rate
 opt = tf.keras.optimizers.Adam(learning_rate=0.001)
 model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
 
-# Инициализация и обучение Tokenizer
 tokenizer = Tokenizer()
 texts = [' '.join([str(word) for word in seq]) for seq in x_train]
 tokenizer.fit_on_texts(texts)
 
-# Обучение модели
 model.fit(x_train, y_train, epochs=5, batch_size=512, validation_data=(x_test, y_test))
 
-# Сохранение модели
 save_model(model, 'my_model_updated_lr.keras')
 
-# Загрузка модели
 model = tf.keras.models.load_model('my_model_updated_lr.keras')
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -59,9 +53,11 @@ def predict():
         sentiment = "Positive" if prediction > 0.5 else "Negative"
         return render_template('index.html', prediction_text=f'The sentiment is {sentiment}')
 
+
 @app.route('/new_comment')
 def new_comment():
     return redirect("http://127.0.0.1:5000/")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
